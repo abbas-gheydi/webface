629bffae89e0658a26098e814d58060b9fb2b881
abbas.gheydi <abbas.gheydi@gmail.com>
2022-11-08 11:20:25 +0330

    add assets to local http server instead of downloading from internet

--- internal/web/router.go
+++ internal/web/router.go
@@ -7,6 +7,7 @@ import (
 
 func Router() {
 	http.HandleFunc("/wf_login/", loginPage)
+	http.Handle("/wf_login/assets/", http.StripPrefix("/wf_login/assets/", http.FileServer(http.Dir("./web/assets"))))
 	http.Handle("/", MustAuth(ProxyRequestHandler(proxy)))
 	log.Printf("start Listenning on %v ... \n", LISTEN_ADDR)
 	log.Fatal(http.ListenAndServe(LISTEN_ADDR, nil))
